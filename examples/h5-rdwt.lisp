163a7c2546835b9367eed55f9b0aa92b9423e6a2
gheber <leberecht@beingandti.me>
2014-08-25 06:36:51 -0500

    A second example.

---
+++ examples/h5-rdwt.lisp
@@ -0,0 +1,28 @@
+;;;; h5-rdwr.lisp
+
+;;; This example illustrates how to write and read data in an existing
+;;; dataset.  It is used in the HDF5 Tutorial.
+
+(in-package :hdf5-cffi)
+
+(let*
+    ((f (h5fopen "dset.h5" '(:rdwr) +H5P-DEFAULT+)) ; file
+     (d (h5dopen2 f "/dset" +H5P-DEFAULT+)))
+
+  ;; initialize the array to be written
+  (with-foreign-object (data :int (* 4 6))
+    (dotimes (i 4)
+      (dotimes (j 6)
+	(setf (mem-aref data :int (+ (* i 6) j)) (+ (* i 6) j 1))))
+
+    ;; write dataset
+    (h5dwrite d +H5T-NATIVE-INT+ +H5S-ALL+ +H5S-ALL+ +H5P-DEFAULT+ data)
+
+    ;; read it back
+    (h5dread d +H5T-NATIVE-INT+ +H5S-ALL+ +H5S-ALL+ +H5P-DEFAULT+ data))
+
+  ;; close all open handles
+  (h5dclose d)
+  (h5fclose f))
+
+(in-package :cl-user)
