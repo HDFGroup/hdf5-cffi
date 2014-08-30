a159ab24a6a90472b694a33e59e538bc517db302
Gerd Heber <leberecht@beingandti.me>
2014-08-30 10:33:11 -0500

    Unwind protection.

--- examples/h5-crtatt.lisp
+++ examples/h5-crtatt.lisp
@@ -2,6 +2,7 @@
 
 ;;; This example illustrates how to create an attribute attached to a
 ;;; dataset. It depends on the HDF5 file created by h5-crtdat.lisp.
+;;; http://www.hdfgroup.org/ftp/HDF5/current/src/unpacked/examples/h5_crtatt.c
 
 (in-package :hdf5-cffi)
 
@@ -16,19 +17,25 @@
         (mem-aref dims 'hsize-t 0) 2)
 
   (let*
-      ((f (h5fopen *FILE* '(:rdwr) +H5P-DEFAULT+))   ; open the file
-       (d (h5dopen2 f "/dset" +H5P-DEFAULT+))        ; open the dataset
-       (s (h5screate-simple 1 dims (null-pointer)))  ; create a shape
-       (a (h5acreate2 d "Units" +H5T-STD-I32BE+ s
-		      +H5P-DEFAULT+ +H5P-DEFAULT+))) ; create an attribute
-
-    ;; write the attribute data
-    (h5awrite a +H5T-NATIVE-INT+ attr-data)
-
-    ;; close all open handles
-    (h5aclose a)
-    (h5sclose s)
-    (h5dclose d)
-    (h5fclose f)))
+      ((fapl (h5pcreate +H5P-FILE-ACCESS+))
+       (file (prog2
+		 (h5pset-fclose-degree fapl :H5F-CLOSE-STRONG)
+		 ;; open the file
+		 (h5fopen *FILE* '(:rdwr) fapl))))
+    (unwind-protect
+	 (let*
+	     ((dset (h5dopen2 file "/dset" +H5P-DEFAULT+))
+	      (shape (h5screate-simple 1 dims (null-pointer)))
+	      ;; create the attribute
+	      (att (h5acreate2 dset "Units" +H5T-STD-I32BE+ shape
+			       +H5P-DEFAULT+ +H5P-DEFAULT+)))
+	   ;; write the attribute data
+	   (h5awrite att +H5T-NATIVE-INT+ attr-data)
+	   (h5aclose att)
+	   (h5sclose shape)
+	   (h5dclose dset))
+      ;; cleanup forms
+      (h5fclose file)
+      (h5pclose fapl))))
 
 (in-package :cl-user)
