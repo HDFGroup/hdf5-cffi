0270c55f08bc31df5975964672e45e7a93d31c3a
Gerd Heber <leberecht@beingandti.me>
2014-08-30 11:49:51 -0500

    Unwind protection.

--- examples/h5-crtgrpar.lisp
+++ examples/h5-crtgrpar.lisp
@@ -1,31 +1,32 @@
 ;;;; h5-crtgrpar.lisp
 
 ;;; This example illustrates the creation of groups using absolute and 
-;;  relative names.
+;;; relative names.
+;;; http://www.hdfgroup.org/ftp/HDF5/current/src/unpacked/examples/h5_crtgrpar.c
 
 (in-package :hdf5-cffi)
 
 (defparameter *FILE* "groups.h5")
 
 (let*
-    ((f (h5fcreate *FILE* '(:trunc) +H5P-DEFAULT+ +H5P-DEFAULT+))
-
-     ;; Create group "MyGroup" in the root group using absolute name.
-     (g1 (h5gcreate2 f "/MyGroup" +H5P-DEFAULT+ +H5P-DEFAULT+
-		     +H5P-DEFAULT+))
-     
-     ;; Create group "Group_A" in group "MyGroup" using absolute name.
-     (g2 (h5gcreate2 f "/MyGroup/Group_A" +H5P-DEFAULT+ +H5P-DEFAULT+
-		     +H5P-DEFAULT+))
-     
-     ;; Create group "Group_B" in group "MyGroup" using relative name.
-     (g3 (h5gcreate2 g1 "Group_B" +H5P-DEFAULT+ +H5P-DEFAULT+
-		    +H5P-DEFAULT+)))
-
-  ;; close all open handles
-  (h5gclose g3)
-  (h5gclose g2)
-  (h5gclose g1)
-  (h5fclose f))
+    ((fapl (h5pcreate +H5P-FILE-ACCESS+))
+     (file (prog2
+	       (h5pset-fclose-degree fapl :H5F-CLOSE-STRONG)
+	       ;; create the file
+	       (h5fcreate *FILE* '(:trunc) +H5P-DEFAULT+ fapl))))
+  (unwind-protect
+       (let*
+	   ((g1 (h5gcreate2 file "/MyGroup" +H5P-DEFAULT+ +H5P-DEFAULT+
+			    +H5P-DEFAULT+))
+	    (g2 (h5gcreate2 file "/MyGroup/Group_A" +H5P-DEFAULT+ +H5P-DEFAULT+
+			    +H5P-DEFAULT+))
+	    (g3 (h5gcreate2 g1 "Group_B" +H5P-DEFAULT+ +H5P-DEFAULT+
+			    +H5P-DEFAULT+)))
+	 (h5gclose g3)
+	 (h5gclose g2)
+	 (h5gclose g1))
+    ;; cleanup forms
+    (h5fclose file)
+    (h5pclose fapl)))
 
 (in-package :cl-user)
