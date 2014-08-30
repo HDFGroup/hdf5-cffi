a8e63e7f09337b2faa54e4ea5886c49f69fb2a84
Gerd Heber <leberecht@beingandti.me>
2014-08-30 10:24:21 -0500

    Unwind protection.

--- examples/h5-rdwt.lisp
+++ examples/h5-rdwt.lisp
@@ -2,6 +2,7 @@
 
 ;;; This example illustrates how to write and read data in an existing
 ;;; dataset. It depends on the HDF5 file created by h5-crtdat.lisp.
+;;; http://www.hdfgroup.org/ftp/HDF5/current/src/unpacked/examples/h5_rdwt.c
 
 (in-package :hdf5-cffi)
 
@@ -9,23 +10,29 @@
 
 (with-foreign-object (dset-data :int (* 4 6))
 
-  ;; initialize the array to be written
-  (flet ((pos (cols i j) (+ (* cols i) j))) ; 2D array access
+  ;; initialize the data to be written
+  (flet ((pos (cols i j) (+ (* cols i) j))) ; 2D array access function
     (dotimes (i 4)
       (dotimes (j 6)
 	(let ((pos (pos 6 i j)))
 	  (setf (mem-aref dset-data :int pos) (1+ pos))))))
 
   (let*
-      ((f (h5fopen *FILE* '(:rdwr) +H5P-DEFAULT+)) ; open the file
-       (d (h5dopen2 f "/dset" +H5P-DEFAULT+)))     ; open the dataset
-
-    ;; write dataset and read it back
-    (h5dwrite d +H5T-NATIVE-INT+ +H5S-ALL+ +H5S-ALL+ +H5P-DEFAULT+ dset-data)
-    (h5dread d +H5T-NATIVE-INT+ +H5S-ALL+ +H5S-ALL+ +H5P-DEFAULT+ dset-data)
-
-    ;; close all open handles
-    (h5dclose d)
-    (h5fclose f)))
+      ((fapl (h5pcreate +H5P-FILE-ACCESS+))
+       (file (prog2
+		 (h5pset-fclose-degree fapl :H5F-CLOSE-STRONG)
+		 ;; open the file
+		 (h5fopen *FILE* '(:rdwr) fapl))))
+    (unwind-protect
+	 (let
+	     ((dset (h5dopen2 file "/dset" +H5P-DEFAULT+))) ; open the dataset
+	   ;; write the dataset
+	   (h5dwrite dset +H5T-NATIVE-INT+ +H5S-ALL+ +H5S-ALL+ +H5P-DEFAULT+ dset-data)
+	   ;; and read it back
+	   (h5dread  dset +H5T-NATIVE-INT+ +H5S-ALL+ +H5S-ALL+ +H5P-DEFAULT+ dset-data)
+	   (h5dclose dset))
+      ;;cleanup forms
+      (h5fclose file)
+      (h5pclose fapl))))
 
 (in-package :cl-user)
