2e3dd53d144ffe96b35d393ac0490e33eddc5f5e
Gerd Heber <leberecht@beingandti.me>
2014-08-30 10:11:27 -0500

    Added unwind protection. (Should be a macro, but this is the low-level interface...)

--- examples/h5-crtdat.lisp
+++ examples/h5-crtdat.lisp
@@ -1,7 +1,7 @@
 ;;;; h5-crtdat.lisp
 
-;;; This example illustrates how to create a dataset that is a 4 x 6 
-;;; array.
+;;; This example illustrates how to create a dataset that is a 4 x 6 array.
+;;; http://www.hdfgroup.org/ftp/HDF5/current/src/unpacked/examples/h5_crtdat.c
 
 (in-package :hdf5-cffi)
 
@@ -9,17 +9,25 @@
 
 (with-foreign-object (dims 'hsize-t 2)
   (let*
-      ((f (h5fcreate *FILE* '(:trunc) +H5P-DEFAULT+ +H5P-DEFAULT+)) ; file
-       (s (progn
-	    (setf (mem-aref dims 'hsize-t 0) 4
-		  (mem-aref dims 'hsize-t 1) 6)
-	    (h5screate-simple 2 dims (null-pointer))))              ; shape
-       (d (h5dcreate2 f "/dset" +H5T-STD-I32BE+ s +H5P-DEFAULT+
-		      +H5P-DEFAULT+ +H5P-DEFAULT+)))                ; dataset
-
-    ;; close all open handles
-    (h5dclose d)
-    (h5sclose s)
-    (h5fclose f)))
+      ((fapl (h5pcreate +H5P-FILE-ACCESS+))
+       (file (prog2
+		 (h5pset-fclose-degree fapl :H5F-CLOSE-STRONG)
+		 ;; create the file
+		 (h5fcreate *FILE* '(:trunc) +H5P-DEFAULT+ fapl))))
+    (unwind-protect
+	 (let*
+	     ((shape (prog2
+		       (setf (mem-aref dims 'hsize-t 0) 4
+			     (mem-aref dims 'hsize-t 1) 6)
+			 ;; create the dataspace
+			 (h5screate-simple 2 dims (null-pointer))))
+	      ;; create the dataset
+	      (dset (h5dcreate2 file "/dset" +H5T-STD-I32BE+ shape +H5P-DEFAULT+
+				+H5P-DEFAULT+ +H5P-DEFAULT+)))
+	   (h5dclose dset)
+	   (h5sclose shape))
+      ;; cleanup forms
+      (h5fclose file)
+      (h5pclose fapl))))
 
 (in-package :cl-user)
