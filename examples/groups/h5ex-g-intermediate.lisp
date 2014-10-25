be7d120a412cd26ffa103d8b51036c77fc9c9723
Gerd Heber <gheber@hdfgroup.org>
2014-10-25 17:33:01 -0500

    Formatting updates.

--- examples/groups/h5ex-g-intermediate.lisp
+++ examples/groups/h5ex-g-intermediate.lisp
@@ -23,23 +23,28 @@
 ;;; the callback function for H5Ovisit
 
 (cffi:defcallback op-func herr-t
-    ((loc-id hid-t)
-     (name :string)
-     (info (:pointer (:struct h5o-info-t)))
+    ((loc-id        hid-t)
+     (name          :string)
+     (info          (:pointer (:struct h5o-info-t)))
      (operator-data :pointer))
-  (prog2
-      (let ((type (cffi:foreign-slot-value info '(:struct h5o-info-t) 'type)))
-	(format t "/")
-	(if (equal name ".")
-	    (format t "  (Group)~%")
-	    (cond ((eql type :H5O-TYPE-GROUP)
-		   (format t "~S  (Group)~%" name))
-		  ((eql type :H5O-TYPE-DATASET)
-		   (format t "~S  (Dataset)~%" name))
-		  ((eql type :H5O-TYPE-NAMED-DATATYPE)
-		   (format t "~S  (Datatype)~%" name))
-		  (t (format t "~S  (Unknown)~%" name)))))
-      0))
+    
+    (prog2
+	(let
+	    ((type (cffi:foreign-slot-value info
+					    '(:struct h5o-info-t)
+					    'type)))
+	  (format t "/")
+	  (if (equal name ".")
+	      (format t "  (Group)~%")
+	      (cond
+		((eql type :H5O-TYPE-GROUP)
+		 (format t "~S  (Group)~%" name))
+		((eql type :H5O-TYPE-DATASET)
+		 (format t "~S  (Dataset)~%" name))
+		((eql type :H5O-TYPE-NAMED-DATATYPE)
+		 (format t "~S  (Datatype)~%" name))
+		(t (format t "~S  (Unknown)~%" name)))))
+	0))
 
 ;;; Showtime
 
@@ -47,27 +52,25 @@
     ((fapl (h5pcreate +H5P-FILE-ACCESS+))
      (file (prog2
 	       (h5pset-fclose-degree fapl :H5F-CLOSE-STRONG)
-	       (h5fcreate *FILE* +H5F-ACC-TRUNC+ +H5P-DEFAULT+ fapl))))
+	       (h5fcreate *FILE* +H5F-ACC-TRUNC+ +H5P-DEFAULT+ fapl)))
+     (gcpl (h5pcreate +H5P-LINK-CREATE+))
+     (group (prog2
+		(h5pset-create-intermediate-group gcpl 1)
+		;; Create the group /G1/G2/G3. Note that /G1 and
+		;; /G1/G2 do not exist yet. This call would cause
+		;; an error if we did not use the previously created
+		;; property list.
+		(h5gcreate2 file "/G1/G2/G3" gcpl
+			    +H5P-DEFAULT+ +H5P-DEFAULT+))))
   
   (unwind-protect
-       
-       (let* ((gcpl (h5pcreate +H5P-LINK-CREATE+))
-	      (group (prog2
-			 (h5pset-create-intermediate-group gcpl 1)
-			 ;; Create the group /G1/G2/G3. Note that /G1 and
-			 ;; /G1/G2 do not exist yet. This call would cause
-			 ;; an error if we did not use the previously created
-			 ;; property list.
-			 (h5gcreate2 file "/G1/G2/G3" gcpl
-				     +H5P-DEFAULT+ +H5P-DEFAULT+))))
-	 (progn
-	   (format t "Objects in the file:~%")
-	   (h5ovisit file :H5-INDEX-NAME :H5-ITER-NATIVE
-		     (cffi:callback op-func) (cffi:null-pointer))
+       (progn
+	 (format t "Objects in the file:~%")
+	 (h5ovisit file :H5-INDEX-NAME :H5-ITER-NATIVE
+		   (cffi:callback op-func) (cffi:null-pointer)))
 	   
-	   (h5gclose group)
-	   (h5pclose gcpl)))    
-
+    (h5gclose group)
+    (h5pclose gcpl)
     (h5fclose file)
     (h5pclose fapl)))
 
