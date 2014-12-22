f4b4ba157a10214341bc3a63768ceb386f79c492
Gerd Heber <gheber@hdfgroup.org>
2014-12-22 08:14:55 -0600

    Updated h5ex-d-alloc.lisp.

--- examples/datasets/h5ex-d-alloc.lisp
+++ examples/datasets/h5ex-d-alloc.lisp
@@ -20,6 +20,7 @@
 
 #+sbcl(require 'asdf)
 (asdf:operate 'asdf:load-op 'hdf5-cffi)
+(asdf:operate 'asdf:load-op 'hdf5-examples)
 
 (in-package :hdf5)
 
@@ -36,7 +37,7 @@
 
 
 (defun print-space-status-et-storage-size (dset name)
-  (cffi:with-foreign-objects ((space-status 'h5d-space-status-t 1))
+  (cffi:with-foreign-object (space-status 'h5d-space-status-t 1)
     (h5dget-space-status dset space-status)
     (format t "Space for ~a has~abeen allocated.~%"
 	    name (if (eql :H5D-SPACE-STATUS-ALLOCATED
@@ -48,16 +49,14 @@
 	    (h5dget-storage-size dset))))
 
 
-(cffi:with-foreign-objects ((dims 'hsize-t 2)
-			    (wdata :int (* *DIM0* *DIM1*)))
+(cffi:with-foreign-object (wdata :int (* *DIM0* *DIM1*))
   ;; initialize data
   (dotimes (i *DIM0*)
     (dotimes (j *DIM1*)
       (setf (cffi:mem-aref wdata :int (pos *DIM1* i j)) (- (* i j) j))))
 
   (let* ((fapl (h5pcreate +H5P-FILE-ACCESS+))
-	 (file (prog2
-		   (h5pset-fclose-degree fapl :H5F-CLOSE-STRONG)
+	 (file (prog2 (h5pset-fclose-degree fapl :H5F-CLOSE-STRONG)
 		   (h5fcreate *FILE* +H5F-ACC-TRUNC+ +H5P-DEFAULT+ fapl))))
     (format t "Creating datasets...~%")
     (format t "~a has allocation time H5D_ALLOC_TIME_LATE~%"
@@ -65,53 +64,39 @@
     (format t "~a has allocation time H5D_ALLOC_TIME_EARLY~%~%"
 	    *DATASET2*)
     (unwind-protect
-	 (progn
-	   (let* ((space
-		   (prog2
-		       (setf (cffi:mem-aref dims 'hsize-t 0) *DIM0*
-			     (cffi:mem-aref dims 'hsize-t 1) *DIM1*)
-		       ;; Create dataspace.  Setting maximum size to NULL
-		       ;; sets the maximum size to be the current size.
-		       (h5screate-simple 2 dims +NULL+)))
-		  (dcpl (h5pcreate +H5P-DATASET-CREATE+))
-		  ;; Create the dataset using the dataset creation property
-		  ;; list.
-		  (dset1 (h5dcreate2 file *DATASET1* +H5T-STD-I32LE+ space
-				     +H5P-DEFAULT+ +H5P-DEFAULT+ +H5P-DEFAULT+))
-		  (dset2
-		   (prog2
-		     ;; Set the allocation time to "early". This way we
-		     ;; can be sure that reading from the dataset
-		     ;; immediately after creation will return the fill value.
-		     (h5pset-alloc-time dcpl :H5D-ALLOC-TIME-EARLY)
-		     (h5dcreate2 file *DATASET2* +H5T-STD-I32LE+ space
-				 +H5P-DEFAULT+ dcpl +H5P-DEFAULT+))))
+         (let* ((space (h5ex:create-simple-dataspace `(,*DIM0* ,*DIM1*)))
+                (dcpl (h5pcreate +H5P-DATASET-CREATE+))
+                ;; Create the dataset using the dataset creation property
+                ;; list.
+                (dset1 (h5dcreate2 file *DATASET1* +H5T-STD-I32LE+ space
+                                   +H5P-DEFAULT+ +H5P-DEFAULT+ +H5P-DEFAULT+))
+                ;; Set the allocation time to "early". This way we
+                ;; can be sure that reading from the dataset
+                ;; immediately after creation will return the fill value.
+                (dset2 (prog2 (h5pset-alloc-time dcpl :H5D-ALLOC-TIME-EARLY)
+                           (h5dcreate2 file *DATASET2* +H5T-STD-I32LE+ space
+                                       +H5P-DEFAULT+ dcpl +H5P-DEFAULT+))))
+           
+           ;; Retrieve and print space status and storage size for dset1.
+           (print-space-status-et-storage-size dset1 *DATASET1*)
+           ;; Retrieve and print space status and storage size for dset2.
+           (print-space-status-et-storage-size dset2 *DATASET2*)
 
-	     ;; Retrieve and print space status and storage size for dset1.
-	     (print-space-status-et-storage-size dset1 *DATASET1*)
-	     ;; Retrieve and print space status and storage size for dset2.
-	     (print-space-status-et-storage-size dset2 *DATASET2*)
+           (format t "~%Writing data...~%~%")
 
-	     (format t "~%Writing data...~%~%")
+           ;; Write the data to the datasets
+           (h5dwrite dset1 +H5T-NATIVE-INT+ +H5S-ALL+ +H5S-ALL+ +H5P-DEFAULT+
+                     wdata)
+           (h5dwrite dset2 +H5T-NATIVE-INT+ +H5S-ALL+ +H5S-ALL+ +H5P-DEFAULT+
+                     wdata)
 
-	     ;; Write the data to the datasets
-	     (h5dwrite dset1 +H5T-NATIVE-INT+ +H5S-ALL+ +H5S-ALL+ +H5P-DEFAULT+
-		       wdata)
-	     (h5dwrite dset2 +H5T-NATIVE-INT+ +H5S-ALL+ +H5S-ALL+ +H5P-DEFAULT+
-		       wdata)
+           ;; Retrieve and print space status and storage size for dset1.
+           (print-space-status-et-storage-size dset1 *DATASET1*)
+           ;; Retrieve and print space status and storage size for dset2.
+           (print-space-status-et-storage-size dset2 *DATASET2*)
 
-	     ;; Retrieve and print space status and storage size for dset1.
-	     (print-space-status-et-storage-size dset1 *DATASET1*)
-	     ;; Retrieve and print space status and storage size for dset2.
-	     (print-space-status-et-storage-size dset2 *DATASET2*)
+           ;; Close and release resources.
+           (h5ex:close-handles (list dset2 dset1 dcpl space)))
+      (h5ex:close-handles (list file fapl)))))
 
-	     ;; Close and release resources.
-	     (h5dclose dset2)
-	     (h5dclose dset1)
-	     (h5pclose dcpl)
-	     (h5sclose space)))
-      
-      (h5fclose file)
-      (h5pclose fapl))))
-
-#+sbcl(sb-ext:quit)
+#+sbcl(sb-ext:exit)
