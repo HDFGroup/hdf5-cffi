59a06f358a54cb0f618bef76202c55fbf69f5f86
Gerd Heber <gheber@hdfgroup.org>
2014-12-22 09:31:54 -0600

    Updated h5ex-d-compact.lisp.

--- examples/datasets/h5ex-d-compact.lisp
+++ examples/datasets/h5ex-d-compact.lisp
@@ -18,6 +18,7 @@
 
 #+sbcl(require 'asdf)
 (asdf:operate 'asdf:load-op 'hdf5-cffi)
+(asdf:operate 'asdf:load-op 'hdf5-examples)
 
 (in-package :hdf5)
 
@@ -26,59 +27,42 @@
 (defparameter *DIM0* 4)
 (defparameter *DIM1* 7)
 
-(defun pos (cols i j)
-  "2D array access function"
-  (+ (* cols i) j))
 
-(cffi:with-foreign-objects ((dims 'hsize-t 2)
-			    (wdata :int (* *DIM0* *DIM1*))
+(cffi:with-foreign-objects ((wdata :int (* *DIM0* *DIM1*))
 			    (rdata :int (* *DIM0* *DIM1*)))
   ;; initialize data
   (dotimes (i *DIM0*)
     (dotimes (j *DIM1*)
-      (setf (cffi:mem-aref wdata :int (pos *DIM1* i j)) (- (* i j) j))))
+      (setf (cffi:mem-aref wdata :int (h5ex:pos2D *DIM1* i j)) (- (* i j) j))))
 
   (let* ((fapl (h5pcreate +H5P-FILE-ACCESS+))
-	 (file (prog2
-		   (h5pset-fclose-degree fapl :H5F-CLOSE-STRONG)
+	 (file (prog2 (h5pset-fclose-degree fapl :H5F-CLOSE-STRONG)
 		   (h5fcreate *FILE* +H5F-ACC-TRUNC+ +H5P-DEFAULT+ fapl))))
     (unwind-protect
-	 (let* ((space
-		 (prog2
-		     (setf (cffi:mem-aref dims 'hsize-t 0) *DIM0*
-			   (cffi:mem-aref dims 'hsize-t 1) *DIM1*)
-		     ;; Create dataspace. Setting maximum size to NULL
-		     ;;sets the maximum size to be the current size.
-		     (h5screate-simple 2 dims +NULL+)))
+	 (let* ((space (h5ex:create-simple-dataspace `(,*DIM0* ,*DIM1*)))
 		(dcpl (h5pcreate +H5P-DATASET-CREATE+))
 		;; Create the dataset using the dataset creation property
 		;; list.
-		(dset
-		 (progn
-		   ;; Create the dataset creation property list, set the
-		   ;; layout to compact.
-		   (h5pset-layout dcpl :H5D-COMPACT)
-		   ;; Create the dataset. We will use all default properties
-		   ;; for this example.
-		   (h5dcreate2 file *DATASET* +H5T-STD-I32LE+ space
-			       +H5P-DEFAULT+ dcpl +H5P-DEFAULT+))))
+		(dset (progn
+                        ;; Create the dataset creation property list, set the
+                        ;; layout to compact.
+                        (h5pset-layout dcpl :H5D-COMPACT)
+                        ;; Create the dataset. We will use all default
+                        ;; properties for this example.
+                        (h5dcreate2 file *DATASET* +H5T-STD-I32LE+ space
+                                    +H5P-DEFAULT+ dcpl +H5P-DEFAULT+))))
 	   ;; Write the data to the dataset.
 	   (h5dwrite dset +H5T-NATIVE-INT+ +H5S-ALL+ +H5S-ALL+ +H5P-DEFAULT+
 		     wdata)
 
 	   ;; Close and release resources.
-	   (h5dclose dset)
-	   (h5pclose dcpl)
-	   (h5sclose space))
-      
-      (h5fclose file)
-      (h5pclose fapl)))
+	   (h5ex:close-handles (list dset dcpl space)))
+      (h5ex:close-handles (list file fapl))))
 
   ;; Now we begin the read section of this example.
 
   (let* ((fapl (h5pcreate +H5P-FILE-ACCESS+))
-	 (file (prog2
-		   (h5pset-fclose-degree fapl :H5F-CLOSE-STRONG)
+	 (file (prog2 (h5pset-fclose-degree fapl :H5F-CLOSE-STRONG)
 		   (h5fopen *FILE* +H5F-ACC-RDONLY+ fapl))))
     (unwind-protect
 	 (let* ((dset (h5dopen2 file *DATASET* +H5P-DEFAULT+))
@@ -100,13 +84,12 @@
 	   (dotimes (i *DIM0*)
 	     (format t " [")
 	     (dotimes (j *DIM1*)
-	       (format t " ~3d" (cffi:mem-aref rdata :int (pos *DIM1* i j))))
+	       (format t " ~3d" (cffi:mem-aref rdata :int
+                                               (h5ex:pos2D *DIM1* i j))))
 	     (format t "]~%"))
 
 	   ;; Close and release resources.
-	   (h5pclose dcpl)
-	   (h5dclose dset))
-      (h5fclose file)
-      (h5pclose fapl))))
+	   (h5ex:close-handles (list dcpl dset)))
+      (h5ex:close-handles (list file fapl)))))
       
-#+sbcl(sb-ext:quit)
+#+sbcl(sb-ext:exit)
